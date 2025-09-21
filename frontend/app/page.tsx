490fd14f47851b59ac0b0fd01352517c1ceab4fe
surfai <166616086+surfai@users.noreply.github.com>
2025-09-21 12:15:57 +0800

    working version

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,20 @@
+"use client";
+
 import { MyAssistant } from "@/components/MyAssistant";
-import {
-  useAssistantInstructions,
-  useAssistantTool,
-} from "@assistant-ui/react";
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { useEdgeRuntime } from "@assistant-ui/react";
 
 export default function Home() {
-  // this is a frontend system prompt that will be made available to the langgraph agent
-  useAssistantInstructions("Your name is assistant-ui.");
-
-  // this is an frontend function that will be made available to the langgraph agent
-  useAssistantTool({
-    toolName: "refresh_page",
-    description: "Refresh the page",
-    parameters: {},
-    execute: async () => {
-      window.location.reload();
-    },
+  const runtime = useEdgeRuntime({
+    api: "http://localhost:8000/api/chat",
+    unstable_AISDKInterop: true,
   });
 
   return (
     <main className="h-dvh">
-      <MyAssistant />
+      <AssistantRuntimeProvider runtime={runtime}>
+        <MyAssistant />
+      </AssistantRuntimeProvider>
     </main>
   );
 }
--- frontend/components/MyAssistant.tsx
+++ frontend/components/MyAssistant.tsx
@@ -1,21 +1,25 @@
-"use client";
-
-import { useEdgeRuntime } from "@assistant-ui/react";
 import { Thread } from "@assistant-ui/react";
 import { makeMarkdownText } from "@assistant-ui/react-markdown";
+import {
+  useAssistantInstructions,
+  useAssistantTool,
+} from "@assistant-ui/react";
 
 const MarkdownText = makeMarkdownText();
 
 export function MyAssistant() {
-  const runtime = useEdgeRuntime({
-    api: "http://localhost:8000/api/chat",
-    unstable_AISDKInterop: true,
+  // this is a frontend system prompt that will be made available to the langgraph agent
+  useAssistantInstructions("Your name is assistant-ui.");
+
+  // this is an frontend function that will be made available to the langgraph agent
+  useAssistantTool({
+    toolName: "refresh_page",
+    description: "Refresh the page",
+    parameters: {},
+    execute: async () => {
+      window.location.reload();
+    },
   });
 
-  return (
-    <Thread
-      runtime={runtime}
-      assistantMessage={{ components: { Text: MarkdownText } }}
-    />
-  );
+  return <Thread assistantMessage={{ components: { Text: MarkdownText } }} />;
 }
